b0e7f8ce01c90eafdc33d4a1f15fb7a4b644622e
Drew Carey Buglione <drewcbuglione@gmail.com>
2013-08-02 11:53:36 -0400

    Add a separate-from-devise Users controller for 'show' action

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,19 @@
+class UsersController < ApplicationController
+  respond_to :html, :json
+
+  before_action :authenticate_user!, only: [:show]
+  before_action :set_user, only: [:show]
+
+  def show
+    respond_with(@user)
+  end
+
+  private
+    def set_user
+      if params[:id] == 'current'
+        @user = current_user
+      else
+        @user = User.find(params[:id])
+      end
+    end
+end
