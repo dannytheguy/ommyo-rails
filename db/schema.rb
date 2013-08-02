9262d41ab8138469ee382198ed1599a5af6d246f
Drew Carey Buglione <drewcbuglione@gmail.com>
2013-08-02 12:50:59 -0400

    Add 'admin' boolean to User

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130802161821) do
+ActiveRecord::Schema.define(version: 20130802164859) do
 
   create_table "brands", force: true do |t|
     t.string   "name"
@@ -20,14 +20,15 @@ ActiveRecord::Schema.define(version: 20130802161821) do
   end
 
   create_table "users", force: true do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "email",                  default: "",    null: false
+    t.string   "encrypted_password",     default: "",    null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "authentication_token"
+    t.boolean  "admin",                  default: false
   end
 
   add_index "users", ["authentication_token"], name: "index_users_on_authentication_token", unique: true
