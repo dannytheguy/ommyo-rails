9a094ed4055850d568bf2b56e59fa89a8c53ffb1
Drew Carey Buglione <drewcbuglione@gmail.com>
2013-08-03 09:51:29 -0400

    Setup ActiveAdmin models

--- app/admin/omm.rb
+++ app/admin/omm.rb
@@ -1,3 +1,3 @@
 ActiveAdmin.register Omm do
-
+  actions :index, :show
 end
--- app/admin/user.rb
+++ app/admin/user.rb
@@ -1,3 +1,9 @@
 ActiveAdmin.register User do
+  actions :index, :show, :edit, :update, :destroy
 
+  controller do
+    def permitted_params
+      params.permit user: [:email]
+    end
+  end
 end
