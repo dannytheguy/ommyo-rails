d188aeb5624d7101154ec85edb644d44d96601f1
Drew Carey Buglione <drewcbuglione@gmail.com>
2013-08-03 09:51:29 -0400

    Setup ActiveAdmin models

--- app/admin/brand.rb
+++ app/admin/brand.rb
@@ -1,3 +1,7 @@
 ActiveAdmin.register Brand do
-
+  controller do
+    def permitted_params
+      params.permit brand: [:name]
+    end
+  end
 end
--- app/admin/omm.rb
+++ app/admin/omm.rb
@@ -1,3 +1,9 @@
 ActiveAdmin.register Omm do
+  actions :index, :show
 
+  controller do
+    def permitted_params
+      params.permit omm: [:body, :user_id, :brand_id]
+    end
+  end
 end
