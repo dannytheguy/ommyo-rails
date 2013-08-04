2f0a9533ee4c6d5353b30cdab331b5713b2c7910
Drew Carey Buglione <drewcbuglione@gmail.com>
2013-08-04 11:43:14 -0400

    UI for users#show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,11 @@ class UsersController < ApplicationController
   before_action :set_user, only: [:show]
 
   def show
+    @omms = @user.omms.paginate(page: params[:page])
+    if current_user == @user
+      @omm = Omm.new
+      @omm.messages.build
+    end
     respond_with(@user)
   end
 
