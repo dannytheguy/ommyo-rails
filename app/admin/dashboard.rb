e35abcf6a06bd271d031839b0fca24487ee9ae53
Drew Carey Buglione <drewcbuglione@gmail.com>
2013-08-22 01:00:18 -0400

    Bring ActiveAdmin up to speed with separate Message model concept

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -8,7 +8,7 @@ ActiveAdmin.register_page "Dashboard" do
         panel "Recent Omms" do
           ul do
             Omm.order("created_at desc").limit(5).map do |omm|
-              li link_to("(#{ time_ago_in_words(omm.created_at) } ago) #{ truncate(omm.body, length: 150, separator: ' ') }", [:admin, omm])
+              li link_to("(#{ time_ago_in_words(omm.created_at) } ago) #{ truncate(omm.messages.last.text, length: 150, separator: ' ') }", [:admin, omm])
             end
           end
         end
---
+++ app/admin/message.rb
@@ -0,0 +1,3 @@
+ActiveAdmin.register Message do
+  actions :index, :show, :destroy
+end
