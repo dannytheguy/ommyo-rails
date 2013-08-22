99ec5af53e3bedc5b044cc1c78bf8e8575d3d127
Drew Carey Buglione <drewcbuglione@gmail.com>
2013-08-22 02:16:43 -0400

    Initial seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,37 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+[
+  { text: 'McDonald’s coffee is slacking today. I want to have something more...',
+    user: 'pdiddy',
+    brand: 'McDonalds' },
+  { text: "I've been waiting in this line for what seems like forever. Starbucks...",
+    user: 'matt543',
+    brand: 'Starbucks' },
+  { text: 'I love the whopper at Burger King...',
+    user: 'johnboy12',
+    brand: 'Burger King' },
+  { text: 'The dealer in Charlotte was rude and not helpful. It...',
+    user: 'rainbowwarrior',
+    brand: 'Chevrolet' },
+  { text: 'Iced coffee at Starbucks gets the people going...',
+    user: 'matt543',
+    brand: 'Starbucks' },
+  { text: 'These delays are killing me. Would it be too much to ask if a real...',
+    user: 'mary-beth',
+    brand: 'American Airlines' }
+].each do |omm|
+  user = User.where(email: "#{omm[:user]}@gmail.com").first_or_create(password: 'password')
+
+  brand = Brand.where(name: omm[:brand]).first_or_create(logo: File.new("#{Rails.root}/db/seeds/#{omm[:brand].parameterize}.png", 'rb'))
+
+  text = omm[:text].sub(/\.{3}\z/, ' lorem ipsum dolor sit amet, consectetur adipisicing elit,
+    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+    irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+    mollit anim id est laborum.')
+  omm = user.omms.build(brand_id: brand.id)
+  omm.messages.build(text: text, author_id: omm.user_id, author_type: 'User')
+  omm.save
+end
+
+User.create(email: 'drew@ommyo.com', password: 'password')
+AdminUser.create(email: 'drew@ommyo.com', password: 'password')
