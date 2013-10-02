09f43baddb39c40e8fa779eee1d6185ace4b13e8
Drew Carey Buglione <drewcbuglione@gmail.com>
2013-10-02 17:28:32 -0400

    Subject line per-Brand Omm index corrections

--- app/mailers/message_mailer.rb
+++ app/mailers/message_mailer.rb
@@ -5,6 +5,6 @@ class MessageMailer < ActionMailer::Base
 
     mail(from: "OmmYo! <#{ EmailIdentifier.assemble(@brand, @message) }>",
          to: "#{@brand.name} <#{@message.omm.brand_email or @brand.email}>",
-         subject: "You've been Omm'd! (##{ @brand.omms.index(@message.omm) })")
+         subject: "You've been Omm'd! (##{ @brand.omms.reverse.index(@message.omm)+1 })")
   end
 end
