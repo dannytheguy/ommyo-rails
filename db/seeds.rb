c43d7fc63256824ecb1ea4093271a3ec4fecd6cb
Drew Carey Buglione <drewcbuglione@gmail.com>
2013-10-09 14:50:31 -0400

    No more line breaks in seed lipsum

--- db/seeds.rb
+++ db/seeds.rb
@@ -34,12 +34,12 @@
 
   brand = Brand.where(name: brand_name).first_or_create(logo: File.new("#{Rails.root}/db/seeds/#{brand_name.parameterize}.png", 'rb'))
 
-  text.sub!(/\.{3}\z/, ' lorem ipsum dolor sit amet, consectetur adipisicing elit,
-    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
-    irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-    mollit anim id est laborum.')
+  text.sub!(/\.{3}\z/, ' lorem ipsum dolor sit amet, consectetur adipisicing elit,'\
+    ' sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,'\
+    ' quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute'\
+    ' irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.'\
+    ' Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt'\
+    ' mollit anim id est laborum.')
 
   omm = user.omms.build(brand_id: brand.id)
   omm.messages.build(text: text, author_id: omm.user_id, author_type: 'User')
