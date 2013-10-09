9be2646a1210b2904a16ca89ea70032556ae1d44
Drew Carey Buglione <drewcbuglione@gmail.com>
2013-10-08 23:12:48 -0400

    Only deliver via IIMs as a fallback

--- app/models/omm_courier.rb
+++ app/models/omm_courier.rb
@@ -5,9 +5,7 @@ class OmmCourier < ActiveRecord::Observer
     if message.author_type == 'User'
       if message.omm.brand_email.present? || message.omm.brand.email.present?
         MessageMailer.outgoing_message(message.omm.brand, message).deliver
-      end
-
-      if message.omm.brand_email.blank? && message.omm.brand.iim.present?
+      elsif message.omm.brand.iim.present?
         Romeo.new.outgoing_message(message.omm.brand, message).deliver
       end
     end
