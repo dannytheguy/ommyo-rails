f857917b373ebdf3673b8574ce59bef4f6f9037f
Drew Carey Buglione <drewcbuglione@gmail.com>
2013-10-09 11:51:34 -0400

    ActiveAdmin Brand view customizations

--- app/admin/brand.rb
+++ app/admin/brand.rb
@@ -16,11 +16,28 @@ ActiveAdmin.register Brand do
     f.inputs "Brand Details" do
       f.input :name
       f.input :email
-      f.input :iim, as: :file, hint: f.template.text_area_tag(nil, f.object.iim, id: nil, disabled: true)
+      f.input :iim, as: :file, hint: f.template.text_area_tag(nil, f.object.iim, id: nil, rows: f.object.iim.lines.count+1, disabled: true)
       f.input :recaptcha_public_key
       f.input :logo, as: :file, hint: f.template.image_tag(f.object.logo.url(:w88h88))
     end
     f.actions
+  end
+
+  show do |s|
+    attributes_table do
+      row :id
+      row :name
+      row :logo do
+        image_tag(s.logo.url(:w88h88))
+      end
+      row :created_at
+      row :updated_at
+      row :recaptcha_public_key
+      row :email
+      row :iim do
+        content_tag 'form', text_area_tag(nil, s.iim, id: nil, rows: s.iim.lines.count+1, disabled: true)
+      end
+    end
   end
 
   controller do
